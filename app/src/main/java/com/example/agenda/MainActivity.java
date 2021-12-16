90382af9a8a8cff052754ca012d105279cbfa7f3
egonlr <egonlr@hotmail.com>
2021-12-16 12:05:45 -0300

    Iniciando a implementação de layout de lista

--- app/src/main/java/com/example/agenda/MainActivity.java
+++ app/src/main/java/com/example/agenda/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.agenda;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
 import org.w3c.dom.Text;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -16,5 +21,13 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Fran", "José"));
+        TextView primeiroAluno = findViewById(R.id.textView);
+        TextView segundoAluno = findViewById(R.id.textView4);
+        TextView terceiroAluno = findViewById(R.id.textView5);
+        primeiroAluno.setText(alunos.get(0));
+        segundoAluno.setText(alunos.get(1));
+        terceiroAluno.setText(alunos.get(2));
     }
 }
