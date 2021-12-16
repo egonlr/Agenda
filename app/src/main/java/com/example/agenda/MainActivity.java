261710403b47d55e350d6b5a134f835b7ea79b78
egonlr <egonlr@hotmail.com>
2021-12-16 10:36:28 -0300

    Implementando o layout da Activity

--- app/src/main/java/com/example/agenda/MainActivity.java
+++ app/src/main/java/com/example/agenda/MainActivity.java
@@ -2,15 +2,19 @@ package com.example.agenda;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(this , "Egon Luiz", Toast.LENGTH_SHORT).show();
+
+        setContentView(R.layout.activity_main);
     }
 }
