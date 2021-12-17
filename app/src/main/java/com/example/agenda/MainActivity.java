a30aa5a1209598476fc78a3fa118ee863d85256b
egonlr <egonlr@hotmail.com>
2021-12-17 11:57:54 -0300

    Aplicando layouts para Activities

--- app/src/main/java/com/example/agenda/MainActivity.java
+++ app/src/main/java/com/example/agenda/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.agenda;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,12 +24,9 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.activity_main);
 
-        List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Fran", "José"));
-        TextView primeiroAluno = findViewById(R.id.textView);
-        TextView segundoAluno = findViewById(R.id.textView4);
-        TextView terceiroAluno = findViewById(R.id.textView5);
-        primeiroAluno.setText(alunos.get(0));
-        segundoAluno.setText(alunos.get(1));
-        terceiroAluno.setText(alunos.get(2));
+        List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Fran", "José", "Pedro", "Maria"));
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_expandable_list_item_1, alunos));
+
     }
 }
